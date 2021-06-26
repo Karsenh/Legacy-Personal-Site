885d0ddf8fb50f5283b8da084085ac9430b78e67
Karsenh <KarsenHansen@gmail.com>
2021-06-25 21:58:51 -0700

    card stlying

--- src/components/Portfolio/Cards/Card.js
+++ src/components/Portfolio/Cards/Card.js
@@ -5,8 +5,8 @@ import React from 'react';
 const Card = ({ title, description, link, platform }) => {
   return (
     <div className='portfolio-card'>
-      <h1 style={{ fontSize: '1.5rem' }}>{title}</h1>
-      <h2 style={{ fontSize: '1rem' }}>{platform}</h2>
+      <h1>{title}</h1>
+      <h2>{platform}</h2>
       <hr style={{ width: '5rem', margin: '1rem auto' }} />
       <p>{description}</p>
       <a href={link}>
